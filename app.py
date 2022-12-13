b3e03c635cebf3a440631e78841d57394d9e1c8f
bg-write <bradywgerber@gmail.com>
2022-12-13 15:43:49 -0600

    add fluxblog newsletter

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from feeds.p4k import p4k
 from feeds.gum import gum
 from feeds.ad import ad
 from feeds.ringer import ringer
-from feeds.flux import flux
+# from feeds.flux import flux
+from feeds.flux_sub import flux_sub
 
 # combining our feeds
-link_dicts = p4k + gum + ad + ringer + flux
+link_dicts = p4k + gum + ad + ringer + flux_sub
 
 # ordering our combined feed by date
 link_dicts_sorted = sorted(link_dicts, key=lambda i: i['date'], reverse=True)
@@ -18,6 +19,26 @@ link_dicts_sorted = sorted(link_dicts, key=lambda i: i['date'], reverse=True)
 link_dicts_sorted_and_reduced = link_dicts_sorted[0:50]
 
 # Universal Time Coordinated (UTC/GMT time)
+'''
+https://www.geeksforgeeks.org/python-datetime-strptime-function/
+WEEKDAYS
+%a = Sun, Mon
+%A = Sunday, Monday
+%w = 0, 1
+DAYS OF THE MONTH
+%d = 01, 02
+%-d = 1, 2
+MONTHS
+%b = Jan, Feb
+%B = January, February
+%m = 01, 02
+%-m = 1, 2
+YEARS
+%y = 99, 00
+%Y = 2000, 1999
+%-y = 0, 99
+HOURS
+'''
 current_date = datetime.now().strftime("%b %d, %Y")
 
 app = Flask(__name__)
--- feeds/flux_sub.py
+++ feeds/flux_sub.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 def get_soup():
     html_text = requests.get(
-        'http://www.fluxblog.org/feed/').text
+        'https://fluxblog.substack.com/feed').text
     return BeautifulSoup(html_text, 'xml')
 
 
@@ -37,14 +37,15 @@ date_list = []
 def deliver_soup():
     for idx, article in enumerate(articles):
         # define our variables (we won't print every single one)
-        article_publication = 'Fluxblog (Blog)'
-        article_RSS = 'http://www.fluxblog.org/feed/'
+        article_publication = 'Fluxblog'
+        article_RSS = 'https://fluxblog.substack.com/feed'
         article_index = idx
         article_title = article.find('title').text
         article_URL = article.find('link').text
         article_date = article.find('pubDate').text
+        # standard time for this app is: %a, %d %b %Y %H:%M:%S %z, but might need to tweak case by case (see flux_sub file for example of change)
         article_date_formatted = datetime.strptime(
-            article_date, "%a, %d %b %Y %H:%M:%S %z")
+            article_date, "%a, %d %b %Y %H:%M:%S %Z")
         use_this_date = article_date_formatted.isoformat()
         article_author = article.find('dc:creator').text
 
@@ -69,7 +70,7 @@ print('soup delivered!')
 # print(date_list)
 
 # combine all my lists into a dict
-flux = [
+flux_sub = [
     {'idx': idx,
      'title': title,
      'URL': URL,
@@ -78,6 +79,6 @@ flux = [
      'date': date}
     for idx, title, URL, author, publication, date in zip(index_list, title_list, URL_list, author_list, publication_list, date_list)
 ]
-# print(flux)
+print(flux_sub)
 
-# python feeds/flux.py
+# python feeds/flux_sub.py
