cd81c2611626250c9143051112971eaa4846dd25
bg-write <bradywgerber@gmail.com>
2022-12-15 09:38:06 -0600

    update pytest requirements

--- feeds/test_feeds.py
+++ feeds/test_feeds.py
@@ -1,9 +1,7 @@
 from p4k import p4k
 
-# the functions we're actually testing
 
-
-def feed(x):
+def feed(x):  # the functions we're actually testing
     return x
 
 
@@ -11,7 +9,6 @@ def test_p4k():  # our testing function
     assert feed(p4k) == p4k
 
 # python feeds/test_feeds.py
-
 # pytest -q
-# remember: fail fast!
+# remember: fail fast
 # todo flesh out all my p4k tests, then move outward
