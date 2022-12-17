12e8b8883601941a086f7947b0c7cba5c10602fb
bg-write <bradywgerber@gmail.com>
2022-12-17 13:56:10 -0600

    add to footer

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ from feeds.billboard_chart_beat import billboard_chart_beat
 from feeds.bandcamp import bandcamp
 
 # combining our feeds
-link_dicts = p4k + gum + ad + ringer + flux_sub + MJI + penny + chi_reader + \
-    uproxx + abundant_living + billboard_chart_beat + bandcamp
+link_dicts = p4k + gum + ad + flux_sub + MJI + penny + chi_reader + \
+    uproxx + abundant_living + billboard_chart_beat + bandcamp + ringer
 
 # ordering our combined feed by date
 link_dicts_sorted = sorted(link_dicts, key=lambda i: i['date'], reverse=True)
