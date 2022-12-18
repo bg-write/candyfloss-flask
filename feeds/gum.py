9ee2036ac879bfe4f96f9353a1e13931cb9363e4
bg-write <bradywgerber@gmail.com>
2022-12-18 13:40:44 -0600

    add gum test

--- feeds/gum.py
+++ feeds/gum.py
@@ -10,18 +10,14 @@ def get_soup():
     return BeautifulSoup(html_text, 'xml')
 
 
-print('getting soup ...')
 soup = get_soup()
-print("soup got!")
 
 
 def cook_soup():  # in our rss link, each stereogum feature is in an "item"
     return soup.find_all('item')
 
 
-print('cooking soup ...')
 articles = cook_soup()
-print('soup cooked!')
 
 # define the empty arrays we'll soon fill up with our loop
 index_list = []
@@ -34,7 +30,8 @@ date_list = []
 
 def deliver_soup():
     for idx, article in enumerate(articles):
-        # define our variables (we won't print every single one)
+        # define our variables
+        # (we won't print every single one)
         article_publication = 'Stereogum'
         article_RSS = 'https://www.stereogum.com/category/reviews/album-of-the-week/feed/'
         article_index = idx
@@ -55,18 +52,9 @@ def deliver_soup():
         date_list.append(use_this_date)
 
 
-print('delivering soup ...')
 deliver_soup()
-print('soup delivered!')
 
-# print(index_list)
-# print(title_list)
-# print(URL_list)
-# print(author_list)
-# print(publication_list)
-# print(date_list)
-
-# combine all my lists into a gum dict
+# combine all my lists into a dict
 gum = [
     {'idx': idx,
      'title': title,
@@ -76,5 +64,4 @@ gum = [
      'date': date}
     for idx, title, URL, author, publication, date in zip(index_list, title_list, URL_list, author_list, publication_list, date_list)
 ]
-
 # python feeds/gum.py
--- feeds/test_feeds.py
+++ feeds/test_feeds.py
@@ -1,4 +1,5 @@
 from p4k import soup, reviews, index_list, title_list, URL_list, author_list, publication_list, date_list, p4k
+from gum import soup, articles, index_list, title_list, URL_list, author_list, publication_list, date_list, gum
 
 
 def test_p4k():
@@ -11,6 +12,18 @@ def test_p4k():
     assert publication_list
     assert date_list
     assert p4k
+
+
+def test_gum():
+    assert soup
+    assert articles
+    assert index_list
+    assert title_list
+    assert URL_list
+    assert author_list
+    assert publication_list
+    assert date_list
+    assert gum
 
 
 '''NOTES ON PYTEST
