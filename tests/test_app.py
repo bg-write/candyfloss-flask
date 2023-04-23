b98e92be9c0ea0fb5cce04bf058a54c550c723dc
bg-write <bradywgerber@gmail.com>
2023-04-23 11:14:43 -0400

    add testing variables

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,18 +10,25 @@ https://google.github.io/styleguide/pyguide.html
 from app import app
 import json
 
+# Defining constants for URLs and status codes
+HOME_URL = '/'
+API_URL = '/api'
+ENDPOINT_URL = '/api/<outlet>'
+UTF_8 = 'utf-8'
+STATUS_OK = 200
+
 
 def test_app_index_route():
     """GET request to app's home page."""
-    response = app.test_client().get('/')
-    assert response.status_code == 200
+    response = app.test_client().get(HOME_URL)
+    assert response.status_code == STATUS_OK
 
 
 def test_app_api_route():
     """GET request to default internal API endpoint."""
-    response = app.test_client().get('/api')
-    assert response.status_code == 200
-    res = json.loads(response.data.decode('utf-8'))
+    response = app.test_client().get(API_URL)
+    assert response.status_code == STATUS_OK
+    res = json.loads(response.data.decode(UTF_8))
     assert type(res) is list
     assert type(res[0]) is dict
     assert type(res[1]) is dict
@@ -29,17 +36,17 @@ def test_app_api_route():
 
 def test_app_api_parameters():
     """GET requests to specified internal API endpoints."""
-    response = app.test_client().get('/api/<outlet>')
-    assert response.status_code == 200
-    res = json.loads(response.data.decode('utf-8'))
+    response = app.test_client().get(ENDPOINT_URL)
+    assert response.status_code == STATUS_OK
+    res = json.loads(response.data.decode(UTF_8))
     assert type(res) is list
 
     response_p4k = app.test_client().get('/api/Pitchfork')
-    res_p4k = json.loads(response_p4k.data.decode('utf-8'))
+    res_p4k = json.loads(response_p4k.data.decode(UTF_8))
     assert type(res_p4k) is list
     assert res_p4k[0]['publication'] == 'Pitchfork'
 
     response_gum = app.test_client().get('/api/Stereogum')
-    res_gum = json.loads(response_gum.data.decode('utf-8'))
+    res_gum = json.loads(response_gum.data.decode(UTF_8))
     assert type(res_gum) is list
     assert res_gum[0]['publication'] == 'Stereogum'
--- tests/test_feeds.py
+++ tests/test_feeds.py
@@ -13,18 +13,22 @@ from feeds.gum import gum
 import requests
 import json
 
+# Defining constants for URLs and status codes
+UTF_8 = 'utf-8'
+STATUS_OK = 200
+
 
 def test_feed_p4k_external():
     '''GET request to Pitchfork's RSS feed'''
     response = requests.get(pitchfork.feed_url)
-    assert response.status_code == 200
+    assert response.status_code == STATUS_OK
 
 
 def test_feed_p4k_internal():
     '''GET request to our Pitchfork API endpoint'''
     response = app.test_client().get('/api/Pitchfork')
     assert response.status_code == 200
-    res_p4k = json.loads(response.data.decode('utf-8'))
+    res_p4k = json.loads(response.data.decode(UTF_8))
     assert type(res_p4k) is list
     assert type(res_p4k[0]) is dict
     assert type(res_p4k[1]) is dict
@@ -35,14 +39,14 @@ def test_feed_stereogum_external():
     '''GET request to Stereogum's RSS feed'''
     # todo need to refactor gum.py to new class structure
     response = requests.get('https://www.stereogum.com/category/music/feed/')
-    assert response.status_code == 200
+    assert response.status_code == STATUS_OK
 
 
 def test_feed_stereogum_internal():
     '''GET request to our Stereogum API endpoint'''
     response = app.test_client().get('/api/Stereogum')
-    assert response.status_code == 200
-    res_gum = json.loads(response.data.decode('utf-8'))
+    assert response.status_code == STATUS_OK
+    res_gum = json.loads(response.data.decode(UTF_8))
     assert type(res_gum) is list
     assert type(res_gum[0]) is dict
     assert type(res_gum[1]) is dict
