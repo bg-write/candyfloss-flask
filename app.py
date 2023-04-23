16c5ea622e7bb1680c7311e045cebe8fb1d326f8
bg-write <bradywgerber@gmail.com>
2023-04-23 10:41:17 -0400

    refactor app for module clarity

--- app.py
+++ app.py
@@ -10,39 +10,47 @@ from flask import Flask, render_template
 import sqlite3
 
 # Local module imports (all our working feeds)
-from feeds.p4k_class import p4k
-from feeds.gum import gum
-from feeds.ad import ad
-from feeds.ringer import ringer
-from feeds.flux_sub import flux_sub
-from feeds.MJI import MJI
-from feeds.penny import penny
-from feeds.chi_reader import chi_reader
-from feeds.uproxx import uproxx
-from feeds.quietus import quietus
 from feeds.abundant_living import abundant_living
-from feeds.billboard_chart_beat import billboard_chart_beat
-from feeds.no_bells import no_bells
-from feeds.loud_quiet import loud_quiet
-from feeds.no_depression import no_depression
-from feeds.sterlewine import so_it_goes
-from feeds.reply_alt import reply_alt
-from feeds.wire import wire_list
-from feeds.passion_of_the_weiss import passion_of_the_weiss_list
-from feeds.nyt import nyt_list
-from feeds.guardian import guardian_list
-from feeds.nme import nme_list
-from feeds.van import van_list
+from feeds.ad import ad
 from feeds.alternative import alternative_list
+# bandcamp
+from feeds.billboard_chart_beat import billboard_chart_beat
+# billboard global
+from feeds.chi_reader import chi_reader
+# creem
+# eight bit theory
+from feeds.flux_sub import flux_sub
+# for columns
+# gq
+from feeds.guardian import guardian_list
+from feeds.gum import gum
+from feeds.loud_quiet import loud_quiet
+from feeds.MJI import MJI
+from feeds.nme import nme_list
+from feeds.no_bells import no_bells
+from feeds.no_depression import no_depression
+from feeds.nyt import nyt_list
 from feeds.ope import ope_list
+from feeds.p4k_class import p4k
+# p4k (original)
+from feeds.passion_of_the_weiss import passion_of_the_weiss_list
+from feeds.penny import penny
+from feeds.quietus import quietus
+from feeds.reply_alt import reply_alt
+from feeds.ringer import ringer
+# spin
+from feeds.sterlewine import so_it_goes
+from feeds.uproxx import uproxx
+from feeds.van import van_list
+# vulture
+from feeds.wire import wire_list
 
 # Combining our feeds into a list of dicts
-link_dicts = (p4k + gum + ad + flux_sub + MJI + penny + chi_reader + uproxx +
-              abundant_living + billboard_chart_beat + ringer + no_bells +
-              quietus + loud_quiet + no_depression + so_it_goes + reply_alt +
-              wire_list + passion_of_the_weiss_list + nyt_list +
-              guardian_list + nme_list + van_list + alternative_list +
-              ope_list)
+link_dicts = (abundant_living + ad + alternative_list + billboard_chart_beat +
+              chi_reader + flux_sub + guardian_list + gum + loud_quiet + MJI +
+              nme_list + no_bells + no_depression + nyt_list + ope_list + p4k +
+              passion_of_the_weiss_list + penny + quietus + reply_alt +
+              ringer + so_it_goes + uproxx + van_list + wire_list)
 '''Ordering our combined feed by date.
 1) pass in our combined feed "link_dicts" to then sort.
 2) Our anonymous function returns our key, which is our date.
@@ -50,8 +58,9 @@ link_dicts = (p4k + gum + ad + flux_sub + MJI + penny + chi_reader + uproxx +
 '''
 link_dicts_sorted = sorted(link_dicts, key=lambda i: i['date'], reverse=True)
 
-# Reducing our feed to return a specific number of articles.
-link_dicts_sorted_and_reduced = link_dicts_sorted[0:50]
+# Reducing our feed to return a specific number of links.
+max_links = 50
+link_dicts_sorted_and_reduced = link_dicts_sorted[0:max_links]
 
 # Universal Time Coordinated (UTC/GMT time)
 current_date = datetime.now().strftime('%b %d, %Y')
